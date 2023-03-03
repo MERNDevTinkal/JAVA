5815fce7cc6196f0ab25bda1b20c0a737f0aa03a
Tinkal Kumar <126617806+MERNDevTinkal@users.noreply.github.com>
2023-03-04 01:09:47 +0530

    Create Array_print.java

---
+++ Array_print.java
@@ -0,0 +1,14 @@
+package array_in_java;
+
+public class Array_print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = new int [11];
+		arr[3] = 43;
+		System.out.println(arr[3]);
+		
+
+	}
+
+}
