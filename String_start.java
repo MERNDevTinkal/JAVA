b6cd7e163023ad0a667c5d8294891cd4e5fd584f
Tinkal Kumar <126617806+MERNDevTinkal@users.noreply.github.com>
2023-03-05 00:24:57 +0530

    Create String_start.java

---
+++ String_start.java
@@ -0,0 +1,15 @@
+package string;
+
+public class String_start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char arr[] = {'a','b','c','d','e','f'};
+		String str = "abcdefgh";
+		System.out.println(str.length());
+		System.out.println(str.charAt(5));
+
+
+	}
+
+}
