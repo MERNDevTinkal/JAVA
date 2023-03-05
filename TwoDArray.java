6a4ca7643d80c31e18904b1af9b31e717a00d35e
Tinkal Kumar <126617806+MERNDevTinkal@users.noreply.github.com>
2023-03-05 23:26:44 +0530

    Create TwoDArray.java

---
+++ TwoDArray.java
@@ -0,0 +1,15 @@
+package twoDArray;
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array2d[][] = new int [3][4];
+		array2d [2][3] = 22;
+		System.out.println(array2d[2][3]);
+		
+		//System.out.println(array2d[2][3]);
+
+	}
+
+}
